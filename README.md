c2bbac8ee7ae2fd28f6f5a400194f98b8fb003f2
Антон Николаев <whiteheron819@yandex.ru>
2021-05-26 13:00:53 +0300

    Pytest complete

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# api_yamdb
-api_yamdb
+# yamdb_final
+yamdb_final
 https://github.com/Whiteheron819/yamdb_final/actions/workflows/main.yml/badge.svg
--- tests/test_yamdb_yml.py
+++ tests/test_yamdb_yml.py
@@ -8,19 +8,19 @@ class TestWorkflow:
 
     def test_workflow(self):
         try:
-            with open(f'{os.path.join(settings.BASE_DIR, "docker-compose.yaml.yaml")}', 'r') as f:
+            with open(f'{os.path.join(settings.BASE_DIR, "yamdb_workflow.yaml")}', 'r') as f:
                 yamdb = f.read()
         except FileNotFoundError:
-            assert False, 'Проверьте, что добавили файл docker-compose.yaml.yaml в корневой каталог для проверки'
+            assert False, 'Проверьте, что добавили файл yamdb_workflow.yaml в корневой каталог для проверки'
 
         assert (
             re.search(r'on:\s*push:\s*branches:\s*-\smaster', yamdb) or
             'on: [push]' in yamdb or
             'on: push' in yamdb 
-        ), 'Проверьте, что добавили действие при пуше в файл docker-compose.yaml.yaml'
-        assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл docker-compose.yaml.yaml'
-        assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл docker-compose.yaml.yaml'
+        ), 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yaml'
+        assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл yamdb_workflow.yaml'
+        assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл yamdb_workflow.yamll'
         assert 'appleboy/telegram-action' in yamdb, (
             'Проверьте, что добавили доставку отправку telegram сообщения '
-            'в файл docker-compose.yaml.yaml'
+            'в файл yamdb_workflow.yaml'
         )
